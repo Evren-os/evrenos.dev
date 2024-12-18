68fa43bc19ae185be94cc3f780bf6091f1a841fb
Evren-os <evrnos@proton.me>
2024-12-18 11:49:18 +0600

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+import type { Links, Site } from "@/types";
+
+export const SITE: Site = {
+  TITLE: "SME",
+  DESCRIPTION: "evrenos's personal website",
+  AUTHOR: "evrenos",
+  REPO_URL: "https://github.com/Evren-os/evrenos.dev",
+};
+
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "About",
+    HREF: "/about",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+];
